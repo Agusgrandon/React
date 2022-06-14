49e65e4a05fb5a902a3ef4e0707baa504e80c685
agustina grandon <agustina107.grandon@gmail.com>
2022-06-14 16:51:03 -0300

    agrege nav

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>React</h1>
-      </header>
+      <h1 className="titulo">//  Titulo  //</h1>
+      <ul className="nav justify-content-center">
+  <li className="nav-item">
+    <a className="nav-link" href="#">Home dos</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Seccion 1</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Seccion 2</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link">Seccion 3</a>
+  </li>
+</ul>
     </div>
   );
 }
